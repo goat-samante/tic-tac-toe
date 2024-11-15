f2fb85d0be482d725538b548fd7fa667fc0d6eac
Samuel Amante <amantsa76@gmail.com>
2024-11-15 09:25:40 -0600

    Initial Commit

---
+++ tic tac toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic tac toe
+//
+//  Created by Samuel Amante on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
